6df015bfd81928bfff7d6c035787ca1b8d5052ad
Nic Ficca <nicficca@gmail.com>
2016-09-08 00:16:39 -0400

    Added forced 10:16 aspect ratio that auto updates collider positions and sizes after window resize

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,17 +5,29 @@ public class Paddle : MonoBehaviour {
 
     // Public
     public float sensitivity;
-    public float xBound;
     public bool canMove = true;
+
+    // Private
+    private float xBound;
+    private BoxCollider2D boxCollider;
+    private int currentScreenWidth;
     
 	// Use this for initialization
 	void Start () {
-
-	}
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
+        currentScreenWidth = Screen.width;
+        UpdateBounds();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (canMove) Move();
+
+        // check for window width change
+        if (currentScreenWidth != Screen.width) {
+            UpdateBounds();
+            currentScreenWidth = Screen.width;
+        }
 	}
 
     // Moves the paddle along the horizontal according to mouse
@@ -29,5 +41,12 @@ public class Paddle : MonoBehaviour {
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -xBound, xBound), transform.position.y, transform.position.z);
     }
 
+    // Updates the x boundary so that the paddle can't move off camera
+    private void UpdateBounds() {
+        Vector3 right = Camera.main.ScreenToWorldPoint(new Vector3(AspectUtility.screenWidth, 0, 0));
+        Vector3 left = Camera.main.ScreenToWorldPoint(Vector3.zero);
+
+        xBound = (right.x - left.x - boxCollider.size.x) / 2.0f;
+    }
 
 }
--- Assets/Scripts/ScreenCollider.cs
+++ Assets/Scripts/ScreenCollider.cs
@@ -8,17 +8,22 @@ public class ScreenCollider : MonoBehaviour {
     // Private
     private Vector3 floorPadding = new Vector3(0, -0.5f, 0);
 
-	// Use this for initialization
-	void Start () {
-        Vector3 bottomLeft = Camera.main.ScreenToWorldPoint(Vector3.zero) + floorPadding;
-        Vector3 topLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 0));
-        Vector3 topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
-        Vector3 bottomRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)) + floorPadding;
-        edgeCollider.points = new Vector2[5] { bottomLeft, topLeft, topRight, bottomRight, bottomLeft };
+    // Use this for initialization
+    void Start () {
+        UpdateEdgePoints();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        
 	}
+
+    // Update the edge collider points so the ball can't leave camera bounds
+    public void UpdateEdgePoints() {
+        Vector3 bottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(AspectUtility.xOffset, AspectUtility.yOffset, 0)) + floorPadding;
+        Vector3 topLeft = Camera.main.ScreenToWorldPoint(new Vector3(AspectUtility.xOffset, AspectUtility.yOffset + AspectUtility.screenHeight, 0));
+        Vector3 topRight = Camera.main.ScreenToWorldPoint(new Vector3(AspectUtility.xOffset + AspectUtility.screenWidth, AspectUtility.yOffset + AspectUtility.screenHeight, 0));
+        Vector3 bottomRight = Camera.main.ScreenToWorldPoint(new Vector3(AspectUtility.xOffset + AspectUtility.screenWidth, AspectUtility.yOffset, 0)) + floorPadding;
+        edgeCollider.points = new Vector2[5] { bottomLeft, topLeft, topRight, bottomRight, bottomLeft };
+    }
 }
